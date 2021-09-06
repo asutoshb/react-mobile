47b931e79ae8b6a44cf91f3fa219303b9d63e2a1
asutoshb <beheraasutosh132@gmail.com>
2021-09-06 22:41:31 +0530

    react-mobile

---
+++ src/components/Mobile.jsx
@@ -0,0 +1,24 @@
+
+function mobile()
+{
+    return (
+        <div>
+            <h2>Mobile Operating system</h2>
+            <ul>
+                <li>Android</li>
+                <li>BlackBerry</li>
+                <li>Windows</li>
+                <li>iphone</li>
+            </ul>
+            <h2>Mobile Manufacturers</h2>
+            <ul>
+                <li>Samsung</li>
+                <li>HTC</li>
+                <li>Micromax</li>
+                <li>Apple</li>
+            </ul>
+        </div>
+    )
+}
+
+export default mobile;
